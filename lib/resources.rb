1add7ea6ca2b1a587ffc29599aa59b146db66433
Hubot <hubot@github.com>
2014-04-01 14:13:40 +0000

    Squashing and merging an update

--- lib/resources.rb
+++ lib/resources.rb
@@ -1207,6 +1207,16 @@ module GitHub
       }
     }
 
+    GIST_FILE_WITH_CONTENT = {
+      "ring.erl" => {
+        "size"     => 932,
+        "raw_url"  => "https://gist.githubusercontent.com/raw/365370/8c4d2d43d178df44f4c03a7f2ac0ff512853564e/ring.erl",
+        "type"     => "text/plain",
+        "language" => "Erlang",
+        "content"  => "contents of gist"
+      }
+    }
+
     GIST = {
       "url"          => "https://api.github.com/gists/#{SecureRandom.hex(10)}",
       "forks_url"    => "https://api.github.com/gists/#{SecureRandom.hex(10)}/forks",
@@ -1226,9 +1236,10 @@ module GitHub
     }
 
     FULL_GIST = GIST.dup.update \
-      :forks   => GIST_FORKS,
-      :history => GIST_HISTORY,
-      :files   => GIST_FILE.merge({'content' => 'contents of gist'})
+      "forks"   => GIST_FORKS,
+      "history" => GIST_HISTORY,
+      "files"   => GIST_FILE_WITH_CONTENT
+
 
     GIST_COMMENT = {
       "id"         => 1,
@@ -1565,7 +1576,7 @@ module GitHub
       "description" => "Deploy request from hubot",
     }
 
-    STATUS = {
+    SIMPLE_STATUS = {
       "created_at" => "2012-07-20T01:19:13Z",
       "updated_at" => "2012-07-20T01:19:13Z",
       "state" => "success",
@@ -1573,7 +1584,23 @@ module GitHub
       "description" => "Build has completed successfully",
       "id" => 1,
       "url" => "https://api.github.com/repos/octocat/example/statuses/1",
+      "context" => "continuous-integration/jenkins"
+    }
+
+    STATUS = SIMPLE_STATUS.merge(
       "creator" => USER
+    )
+
+    COMBINED_STATUS = {
+      "state" => "success",
+      "name"  => "octocat/Hello-World",
+      "sha"   => COMMIT["sha"],
+      "statuses" => [
+        SIMPLE_STATUS.merge("context" => "continuous-integration/jenkins"),
+        SIMPLE_STATUS.merge("context" => "security/brakeman")
+      ],
+      "commit_url" => "https://api.github.com/repos/octocat/Hello-World/#{COMMIT["sha"]}",
+      "repository_url" => "https://api.github.com/repos/octocat/Hello-World"
     }
 
     META = {
